419d9e2b75d47ee1b7f8c1cdb139cdf4c8013012
turbo-ismam <turboismam@gmail.com>
2023-02-19 19:13:01 +0100

    feat: add catalog item and test

--- src/main/scala/stores/store/valueobjects/CatalogItem.scala
+++ src/main/scala/stores/store/valueobjects/CatalogItem.scala
@@ -7,9 +7,26 @@
 package io.github.pervasivecats
 package stores.store.valueobjects
 
-import stores.Id
+import stores.{Id, Validated, ValidationError}
+
+import eu.timepit.refined.api.RefType.applyRef
 
 trait CatalogItem {
 
   val id: Id
+}
+
+object CatalogItem {
+
+  final private case class CatalogItemImpl(id: Id) extends CatalogItem
+
+  case object WrongCatalogItemIdFormat extends ValidationError {
+
+    override val message: String = "The catalog item id is a negative value"
+  }
+
+  def apply(value: Long): Validated[CatalogItem] = applyRef[Id](value) match {
+    case Left(_) => Left[ValidationError, CatalogItem](WrongCatalogItemIdFormat)
+    case Right(value) => Right[ValidationError, CatalogItem](CatalogItemImpl(value))
+  }
 }
--- src/test/scala/stores/store/valueobjects/CatalogItemTest.scala
+++ src/test/scala/stores/store/valueobjects/CatalogItemTest.scala
@@ -3,30 +3,30 @@ package stores.store.valueobjects
 
 import org.scalatest.funspec.AnyFunSpec
 import eu.timepit.refined.auto.given
-import stores.store.valueobjects.ItemId.WrongItemIdFormat
+import stores.store.valueobjects.CatalogItem.WrongCatalogItemIdFormat
 import org.scalatest.EitherValues.given
 import org.scalatest.matchers.should.Matchers.*
 
-class ItemIdTest extends AnyFunSpec {
+class CatalogItemTest extends AnyFunSpec {
 
-  describe("An item id") {
+  describe("A catalog item") {
     describe("when created with a negative value") {
       it("should not be valid") {
-        ItemId(-9000).left.value shouldBe WrongItemIdFormat
+        CatalogItem(-9000).left.value shouldBe WrongCatalogItemIdFormat
       }
     }
 
     describe("when created with a positive value") {
       it("should be valid") {
         val id: Long = 9000
-        (ItemId(id).value.value: Long) shouldBe id
+        (CatalogItem(id).value.id: Long) shouldBe id
       }
     }
 
     describe("when created with a value of 0") {
       it("should be valid") {
         val id: Long = 0
-        (ItemId(id).value.value: Long) shouldBe id
+        (CatalogItem(id).value.id: Long) shouldBe id
       }
     }
   }
