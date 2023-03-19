6df7815d0b1e82fd347e5a0558b9f4abdb5b60a2
aismam <ismamabu25@gmail.com>
2023-03-19 18:17:59 +0100

    fix: update updateShelvingGroup method in Store

--- src/main/scala/stores/store/entities/Store.scala
+++ src/main/scala/stores/store/entities/Store.scala
@@ -8,6 +8,7 @@ package io.github.pervasivecats
 package stores.store.entities
 
 import AnyOps.*
+import stores.store.entities.StoreOps.*
 import stores.store.valueobjects.{ShelvingGroup, ShelvingGroupId, StoreId}
 
 trait Store {
@@ -23,11 +24,16 @@ object Store {
 
   given StoreOps[Store] with {
 
-    override def addShelvingGroup(store: Store, shelvingGroup: ShelvingGroup): Store = StoreImpl(store.storeId, store.layout ++ List[ShelvingGroup](shelvingGroup))
+    override def addShelvingGroup(store: Store, shelvingGroup: ShelvingGroup): Store =
+      StoreImpl(store.storeId, store.layout ++ List[ShelvingGroup](shelvingGroup))
 
-    override def removeShelvingGroup(store: Store, shelvingGroupId: ShelvingGroupId): Store = StoreImpl(store.storeId, store.layout.filter(_.shelvingGroupId !== shelvingGroupId))
+    override def removeShelvingGroup(store: Store, shelvingGroupId: ShelvingGroupId): Store =
+      StoreImpl(store.storeId, store.layout.filter(_.shelvingGroupId !== shelvingGroupId))
 
-    override def updateShelvingGroup(store: Store, shelvingGroup: ShelvingGroup): Store = StoreImpl(store.storeId, store.layout.filter(_.shelvingGroupId !== shelvingGroup.shelvingGroupId) ++ List[ShelvingGroup](shelvingGroup))
+    override def updateShelvingGroup(store: Store, shelvingGroup: ShelvingGroup): Store =
+      store
+        .removeShelvingGroup(shelvingGroup.shelvingGroupId)
+        .addShelvingGroup(shelvingGroup)
   }
 
   def apply(storeId: StoreId, layout: List[ShelvingGroup]): Store = StoreImpl(storeId, layout)
