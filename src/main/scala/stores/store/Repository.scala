7dc42ade567f95278d6fa73bda7ef36225d3e69e
aismam <ismamabu25@gmail.com>
2023-03-27 23:07:18 +0200

    refactor: scalafmt and scalafixall

--- src/main/scala/stores/store/Repository.scala
+++ src/main/scala/stores/store/Repository.scala
@@ -7,17 +7,31 @@
 package io.github.pervasivecats
 package stores.store
 
-import stores.store.valueobjects.{CatalogItem, Count, ItemId, ItemsRow, ItemsRowId, Shelf, ShelfId, Shelving, ShelvingGroup, ShelvingGroupId, ShelvingId, StoreId}
-import stores.store.entities.Store
-import stores.{Validated, ValidationError}
-
 import scala.util.Try
+
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
 import eu.timepit.refined.auto.given
 import io.getquill.*
 import io.getquill.autoQuote
+
+import stores.store.valueobjects.{
+  CatalogItem,
+  Count,
+  ItemId,
+  ItemsRow,
+  ItemsRowId,
+  Shelf,
+  ShelfId,
+  Shelving,
+  ShelvingGroup,
+  ShelvingGroupId,
+  ShelvingId,
+  StoreId
+}
+import stores.store.entities.Store
+import stores.{Validated, ValidationError}
 import AnyOps.{!==, ===}
 
 trait Repository {
@@ -49,7 +63,15 @@ object Repository {
 
     import ctx.*
 
-    private case class ItemsRows(storeId: Long, shelvingGroupId: Long, shelvingId: Long, shelfId: Long, itemsRowId: Long, catalogItem: Long, count: Int)
+    private case class ItemsRows(
+      storeId: Long,
+      shelvingGroupId: Long,
+      shelvingId: Long,
+      shelfId: Long,
+      itemsRowId: Long,
+      catalogItem: Long,
+      count: Int
+    )
 
     private def protectFromException[A](f: => Validated[A]): Validated[A] =
       Try(f).getOrElse(Left[ValidationError, A](RepositoryOperationFailed))
@@ -57,46 +79,79 @@ object Repository {
     override def findById(storeId: StoreId): Validated[Store] = protectFromException {
       ctx
         .run(query[ItemsRows].filter(_.storeId === lift[Long](storeId.value)))
-        .map(i => for {
-          itemsRowId <- ItemsRowId(i.itemsRowId)
-          catalogItem <- CatalogItem(i.catalogItem)
-          count <- Count(i.count)
-          shelfId <- ShelfId(i.shelfId)
-          shelvingId <- ShelvingId(i.shelvingId)
-          shelvingGroupId <- ShelvingGroupId(i.shelvingGroupId)
-        } yield (shelvingGroupId, shelvingId, shelfId, itemsRowId, catalogItem, count))
-        .foldLeft[Either[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]]](Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](StoreNotFound)){
-          case (Right(s), Right(i)) => Right[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](s :+ i)
-          case (Right(_), Left(v)) => Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](v)
-          case (Left(v), _) => Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](v)
+        .map(i =>
+          for {
+            itemsRowId <- ItemsRowId(i.itemsRowId)
+            catalogItem <- CatalogItem(i.catalogItem)
+            count <- Count(i.count)
+            shelfId <- ShelfId(i.shelfId)
+            shelvingId <- ShelvingId(i.shelvingId)
+            shelvingGroupId <- ShelvingGroupId(i.shelvingGroupId)
+          } yield (shelvingGroupId, shelvingId, shelfId, itemsRowId, catalogItem, count)
+        )
+        .foldLeft[Either[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]]](
+          Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](StoreNotFound)
+        ) {
+          case (Right(s), Right(i)) =>
+            Right[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](s :+ i)
+          case (Right(_), Left(v)) =>
+            Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](v)
+          case (Left(v), _) =>
+            Left[ValidationError, Seq[(ShelvingGroupId, ShelvingId, ShelfId, ItemsRowId, CatalogItem, Count)]](v)
         }
-        .map(s => Store(storeId, s.groupBy(_._1)
-          .toSeq
-          .map((shelvingGroupId, shelvings) => ShelvingGroup(shelvingGroupId, shelvings.groupBy(_._2).toSeq.map((shelvingId, shelves) => Shelving(shelvingId, shelves.groupBy(_._3).toSeq.map((shelfId, itemsRows) => Shelf(shelfId, itemsRows.map(i => ItemsRow(i._4, i._5, i._6))))))))))
+        .map(s =>
+          Store(
+            storeId,
+            s.groupBy(_._1)
+              .toSeq
+              .map((shelvingGroupId, shelvings) =>
+                ShelvingGroup(
+                  shelvingGroupId,
+                  shelvings
+                    .groupBy(_._2)
+                    .toSeq
+                    .map((shelvingId, shelves) =>
+                      Shelving(
+                        shelvingId,
+                        shelves
+                          .groupBy(_._3)
+                          .toSeq
+                          .map((shelfId, itemsRows) => Shelf(shelfId, itemsRows.map(i => ItemsRow(i._4, i._5, i._6))))
+                      )
+                    )
+                )
+              )
+          )
+        )
 
     }
 
     override def updateLayout(store: Store, layout: Seq[ShelvingGroup]): Validated[Unit] = protectFromException {
       ctx
         .transaction {
-          if(ctx.run(query[ItemsRows].filter(_.storeId === lift[Long](store.storeId.value)).delete) <= 0)
+          if (ctx.run(query[ItemsRows].filter(_.storeId === lift[Long](store.storeId.value)).delete) <= 0)
             Left[ValidationError, Unit](RepositoryOperationFailed)
           else if (
             ctx
-              .run(liftQuery(
-                for {
-                  shelvingGroup <- layout
-                  shelving <- shelvingGroup.shelvings
-                  shelf <- shelving.shelves
-                  itemsRow <- shelf.itemsRows
-                } yield ItemsRows(store.storeId.value,
-                  shelvingGroup.shelvingGroupId.value,
-                  shelving.shelvingId.value,
-                  shelf.shelfId.value,
-                  itemsRow.itemsRowId.value,
-                  itemsRow.catalogItem.id,
-                  itemsRow.count.value)
-              ).foreach(i => query[ItemsRows].insertValue(i))).forall(_===1L)
+              .run(
+                liftQuery(
+                  for {
+                    shelvingGroup <- layout
+                    shelving <- shelvingGroup.shelvings
+                    shelf <- shelving.shelves
+                    itemsRow <- shelf.itemsRows
+                  } yield ItemsRows(
+                    store.storeId.value,
+                    shelvingGroup.shelvingGroupId.value,
+                    shelving.shelvingId.value,
+                    shelf.shelfId.value,
+                    itemsRow.itemsRowId.value,
+                    itemsRow.catalogItem.id,
+                    itemsRow.count.value
+                  )
+                ).foreach(i => query[ItemsRows].insertValue(i))
+              )
+              .forall(_ === 1L)
           )
             Right[ValidationError, Unit](())
           else
